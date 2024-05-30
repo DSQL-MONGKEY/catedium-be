326cbb13a18fbc78a5dc64de3d5203940308ceed
Dimpfe <dimasprasetyo7696@gmail.com>
2024-05-30 21:57:52 +0700

    feat<>: add classifyCat into route event handler

--- routes/catRoutes.js
+++ routes/catRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer();
+const { classifyCat } = require('../controllers/catController');
 
 
-router.post('/classify', upload.single('image'))
+router.post('/classify', upload.single('image'), classifyCat);
 
 module.exports = router;
